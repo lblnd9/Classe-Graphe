edb90a72e31b3fad83f8e50099ee7ee0d5973002
LyceeCurieVire <55030663+LyceeCurieVire@users.noreply.github.com>
2021-01-25 21:56:51 +0100

    Create UnionFind.py

---
+++ UnionFind.py
@@ -0,0 +1,13 @@
+class UnionFind:
+    """
+    La classe UnionFind permet de gérer la structure du même nom, définie par son constructeur
+    qui à un chaque élément d'un énumérable associe un numéro, et pas les deux primitives
+    Union qui réunit deux parties de la partition et Find qui donne le numéro de la partition
+    à laquelle appartient un élément donné.
+    """
+    
+    def __init__(self,enumerable):
+    
+    def Union(self,p1,p2):
+    
+    def Find(self,p):
