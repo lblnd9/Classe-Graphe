d60a8e87153410ae1abdad221e2b84c09e9a932b
LyceeCurieVire <55030663+LyceeCurieVire@users.noreply.github.com>
2021-01-25 21:59:56 +0100

    Create Heap.py

---
+++ Heap.py
@@ -0,0 +1,15 @@
+class Heap:
+    """
+    La structure de tas binaire minimal est définie par ses primitives :
+    * un constructeur crée un tas vide
+    * entasse ajoute un élément dans le tas, en conservant la structure de tas, 
+    * minimum renvoie la racine, qui est le minimum des éléments du tas,
+    en conservant la structure de tas.
+    """
+    
+    def __init__(self):
+    
+    def entasse(self,e):
+    
+    def minimum(self):
+        return
