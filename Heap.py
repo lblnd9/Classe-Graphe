5ee65d3103fe133c89954909ee6b50183b15f103
Frexom <77272835+Frexom@users.noreply.github.com>
2021-01-30 11:03:12 +0100

    Update Heap.py
    Ajout des fonctions __init__(), entasse() et minimum().

--- Heap.py
+++ Heap.py
@@ -8,8 +8,40 @@ class Heap:
     """
     
     def __init__(self):
+        '''
+        Fonction par Hugo MARTIN, crée un tas binaire.
+        '''
+        sekf.heap = []
     
     def entasse(self,e):
-    
+        '''
+        Fonction par Hugo MARTIN, insère un élément dans le tas binaire.
+        '''
+        self.heap.append(n)
+        index_n = len(self.heap)-1
+        while n < self.heap[int((index_n-1)/2)]:
+            self.heap[index_n], self.heap[int((index_n-1)/2)] = self.heap[int((index_n-1)/2)],self.heap[index_n]
+            index_n = int((index_n-1)/2)    
+            
+            
     def minimum(self):
-        return
+        '''
+        Fonction par Hugo MARTIN, renvoie le plus petit élément du tas binaire minimal.
+        '''
+        r_value = self.heap[0]
+        self.heap[0] = self.heap[-1]
+        self.heap = self.heap[:-1]
+        n = self.heap[0]
+        index_n=0
+        while (len(self.heap)-1 >= 2*index_n+1 and n > self.heap[2*index_n +1]) or (len(self.heap)-1 >= 2*index_n+2 and n > self.heap[2*index_n +2]):
+            if len(self.heap)-1 == 2*index_n +1: #un seul fils
+                change_index = 2*index_n +1
+            else:                                #deux fils
+                if self.heap[2*index_n +1] > self.heap[2*index_n +2]:
+                    change_index = 2*index_n +2
+                else:
+                    change_index = 2*index_n +1
+            self.heap[index_n],self.heap[change_index] = self.heap[change_index],self.heap[index_n]
+            index_n = change_index
+            n = self.heap[index_n]
+        return r_value
