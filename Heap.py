312afeb41ce41fd4b96856662b464251d4938729
Frexom <77272835+Frexom@users.noreply.github.com>
2021-02-15 16:51:06 +0100

    Update Heap.py
    Changement de la représentation des éléments dans le Heap, on passe d'un nombre seul, à une liste comprenant le poids le l'arrête, et le nom des deux points que cette dernière relie.
    Ex : [3, 'C', 'D']

--- Heap.py
+++ Heap.py
@@ -19,29 +19,34 @@ class Heap:
         '''
         self.heap.append(n)
         index_n = len(self.heap)-1
-        while n < self.heap[int((index_n-1)/2)]:
+        while n[0] < self.heap[int((index_n-1)/2)][0]:
             self.heap[index_n], self.heap[int((index_n-1)/2)] = self.heap[int((index_n-1)/2)],self.heap[index_n]
-            index_n = int((index_n-1)/2)    
+            index_n = int((index_n-1)/2)   
             
             
     def minimum(self):
         '''
         Fonction par Hugo MARTIN, renvoie le plus petit élément du tas binaire minimal.
         '''
+        if self.heap == []:
+            return None
         r_value = self.heap[0]
         self.heap[0] = self.heap[-1]
         self.heap = self.heap[:-1]
-        n = self.heap[0]
+        if self.heap == []:
+            return r_value
+        n = self.heap[0][0]
         index_n=0
-        while (len(self.heap)-1 >= 2*index_n+1 and n > self.heap[2*index_n +1]) or (len(self.heap)-1 >= 2*index_n+2 and n > self.heap[2*index_n +2]):
+        while (len(self.heap)-1 >= 2*index_n+1 and n > self.heap[2*index_n +1][0]) or (len(self.heap)-1 >= 2*index_n+2 and n > self.heap[2*index_n +2][0]):
             if len(self.heap)-1 == 2*index_n +1: #un seul fils
                 change_index = 2*index_n +1
             else:                                #deux fils
-                if self.heap[2*index_n +1] > self.heap[2*index_n +2]:
-                    change_index = 2*index_n +2
+                if self.heap[2*index_n +1][0] > self.heap[2*index_n +2][0]:
+                    change_index = 2*index_n + 2
                 else:
                     change_index = 2*index_n +1
             self.heap[index_n],self.heap[change_index] = self.heap[change_index],self.heap[index_n]
             index_n = change_index
-            n = self.heap[index_n]
+            n = self.heap[index_n][0]
         return r_value
+
