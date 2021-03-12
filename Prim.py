c986f240490ca956dc92a1fdb53a578fc51c4765
Frexom <77272835+Frexom@users.noreply.github.com>
2021-03-12 15:26:49 +0100

    Update Prim.py
    Correction de la docstring de la fonction Prim(), amélioration notable de sa complexité et ajout de commentaires.

--- Prim.py
+++ Prim.py
@@ -1,15 +1,15 @@
 from Heap import Heap
-import copy
 
 def Prim(graphe,s):
     '''
-    Fontion Réalisée par Hugo MARTIN.
-    La fonction Prim prend en paramètre une liste d'adjascence comme
-    représentation d'un graphe et un sommet présent dans le grapphe,
-    et revoie un arbre couvrant minimal du graphe créé suivant la méthode de
-    l'agorithme de prim donné. Les doublons d'arrêtes ne sont pas renvoyés.
+    La fonction Prim prend en paramètres une liste d'adjascence comme
+    représentation d'un graphe et un sommet présent dans le graphe,
+    et elle revoie un arbre couvrant minimal du graphe créé suivant la méthode
+    de l'agorithme de prim donné. Les doublons d'arrêtes ne sont pas renvoyés.
     La fonction renverra un dictionnaire vide si le graphe est de longueur
     nulle/1 ou si le sommet passé en paramètre n'appartient pas au graphe.
+    Fonction Réalisée par Hugo MARTIN.
+    
     >>> Prim({'A':[('B',5),('C',7)], 'B':[('C',8)], 'C':[('D',3)], 'D':[]},'A')
     {'A': [('B', 5), ('C', 7)], 'B': [], 'C': [('D', 3)], 'D': []}
     >>> Prim({'A':[('B',5),('C',7)], 'B':[('C',8)], 'C':[('D',3)], 'D':[]},'M')
@@ -19,28 +19,32 @@ def Prim(graphe,s):
     >>> Prim({},'A')
     {}
     '''
-    if len(graphe) == 0 or len(graphe) == 1 or s not in graphe.keys():
+    if len(graphe) == 0 or len(graphe) == 1 or s not in graphe.keys():          #Cas de base
         return {}
     acm = {}
     f = Heap()
-    len_f = 0
     for i in graphe.keys():
-        acm[i] = []
+        acm[i] = []                                                             #Création du graphe à renvoyer
         for j in range(len(graphe[i])):
-            f.insert([graphe[i][j][1],i,graphe[i][j][0]])
-            len_f += 1
+            f.insert([graphe[i][j][1],i,graphe[i][j][0]])                       #Création du Heap
 
-    heap = copy.deepcopy(f)
     vus = [s]
-    while len(vus) != len(graphe):
+    waiting = []
+    while len(vus) != len(graphe):                                              #Exploitation du Heap pour résoudre le problème
         value = f.shorten()
-        if (value[1] in vus) != (value[2] in vus):
+        if value == None:                                                           #i.e Si le Heap est vide
+            for elem in waiting:
+                f.insert(elem)
+            waiting = []
+        elif (value[1] in vus) != (value[2] in vus):                                #i.e Sinon si un sommest est dans vus et l'autre non
             acm[value[1]].append((value[2],value[0]))
-            f = copy.deepcopy(heap)
             if value[1] in vus:
                 vus.append(value[2])
             else:
                 vus.append(value[1])
+        elif (not value[1] in vus) and (not value[2] in vus):                       #sinon si aucun des deux sommets n'est dans vus (arrête encore inaccesible)
+            waiting.append(value)
+
     return acm
 		
 if __name__ == "__main__":
